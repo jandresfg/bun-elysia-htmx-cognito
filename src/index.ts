b7abb51a23e2e72e88886e43daa332e7ff7db02f
Andres Fernandez <jandresfgz@gmail.com>
2023-09-13 11:56:55 -0500

    feat: use html plugin to render a base html

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
+import html from "@elysiajs/html";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3333);
+const app = new Elysia()
+  .use(html())
+  .get("/", ({ html }) => html(baseHtml))
+  .listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+const baseHtml = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>THE BETH STACK</title>
+  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
+  <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
+  <link href="/styles.css" rel="stylesheet">
+</head>
+
+<body>Hello base html</body>
+`;
