e325c7d0ba787318b41000bf43019d8b44ffdbd6
Andres Fernandez <jandresfgz@gmail.com>
2023-09-13 12:15:38 -0500

    feat: add jsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,28 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .use(html())
-  .get("/", ({ html }) => html(baseHtml))
+  .get("/", ({ html }) =>
+    html(
+      <baseHtml>
+        <h1>Hello jsx</h1>
+      </baseHtml>
+    )
+  )
   .listen(3333);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
 
-const baseHtml = `
+const BaseHtml = ({ children }) => `
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>THE BETH STACK</title>
-  <script src="https://unpkg.com/htmx.org@1.9.3"></script>
-  <script src="https://unpkg.com/hyperscript.org@0.9.9"></script>
-  <link href="/styles.css" rel="stylesheet">
+  <title>Bun + Elysia + htmx + cognito</title>
 </head>
 
-<body>Hello base html</body>
+<body>${children}</body>
 `;
