e25d6d428d95d6658d15ecb70be24dafd30b7816
Andres Fernandez <jandresfgz@gmail.com>
2023-09-15 10:54:46 -0500

    chore: remove amazon-cognito-identity-js

--- src/UserPool.ts
+++
@@ -1,9 +0,0 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-const poolData = {
-  // personal
-  UserPoolId: "us-east-2_G9iAT1N63",
-  ClientId: "3393ih1cu96j3n7hm2dfdjtd55",
-};
-
-export default new CognitoUserPool(poolData);
